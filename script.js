f3d7fdc1a54ce5d8bcb4da7255e86228096e2500
Rachel Fingerhut <fingerhutrachel@gmail.com>
2025-10-22 20:21:10 -0700

    feat(new grid button): Create button where user can input number of boxes new grid will have.

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ for (let j = 0; j<16; j++){
     }
     gridContainer.appendChild(row);
 }
+const createNewGridBtn = document.querySelector('#createNewGridBtn');
+createNewGridBtn.addEventListener('click', () => {
+    let numBoxes = ''
+    let msg = `Enter the number of boxes you'd like in your grid. Must be 100 or less!`
+    do{
+        numBoxes = Number(prompt(msg));
+        if (numBoxes > 100 || isNaN(numBoxes) || numBoxes <= 0){
+            msg = `Please enter a number that is less than or equal to 100!`;
+        }
+    } while (numBoxes > 100 || isNaN(numBoxes) || numBoxes <= 0)
+    alert(`Number of boxes will be ${numBoxes}`);
+});
+
+
+
+function newGrid(){
+    
+   
+}
