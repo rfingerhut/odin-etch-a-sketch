cb7eadeaee0af6d97ecef916e532c3aa28959fb2
Rachel Fingerhut <fingerhutrachel@gmail.com>
2025-10-22 20:28:10 -0700

    feat(new grid button): Creates new grid based on user input. Replaces the old grid.

--- script.js
+++ script.js
@@ -11,22 +11,38 @@ for (let j = 0; j<16; j++){
     }
     gridContainer.appendChild(row);
 }
+
 const createNewGridBtn = document.querySelector('#createNewGridBtn');
 createNewGridBtn.addEventListener('click', () => {
     let numBoxes = ''
-    let msg = `Enter the number of boxes you'd like in your grid. Must be 100 or less!`
+    let msg = `Enter the number of rows you'd like in your grid. Must be 100 or less!`
     do{
         numBoxes = Number(prompt(msg));
         if (numBoxes > 100 || isNaN(numBoxes) || numBoxes <= 0){
             msg = `Please enter a number that is less than or equal to 100!`;
         }
     } while (numBoxes > 100 || isNaN(numBoxes) || numBoxes <= 0)
-    alert(`Number of boxes will be ${numBoxes}`);
+    clearGrid();
+    newGrid(numBoxes);
+
 });
 
+function clearGrid(){
+    gridContainer.replaceChildren();
+}
 
 
-function newGrid(){
-    
-   
+
+function newGrid(numBoxes){
+    for (let j = 0; j<numBoxes; j++){
+        const row = document.createElement('div');
+        for (let i = 0; i<numBoxes; i++){
+            const column = document.createElement('div');
+            column.classList.add('gridBox');
+            column.id = `box${j}-${i}`;
+            column.textContent=`${j},${i}`;
+            row.appendChild(column);
+        }
+        gridContainer.appendChild(row);
+    }
 }
